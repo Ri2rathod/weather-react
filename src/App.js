6ffbc50af034c6d3b92e7caab73c0abfd91bf724
Ritesh <rathodriteshrock@gmail.com>
2020-12-16 21:05:27 +0530

    Weather Components Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container mt-5">
+        <div className="row">
+            <div className="col-12 col-sm-8 col-md-6 bg-dark mx-auto weather_card">
+                <div className="container-fluid ">
+                    <div className="header py-5">
+                        <h1 className="display-6 text-white text-center">Bilimora, Gujarat</h1>
+                        <h4 className=" text-white text-center">Haze Sunshine</h4>
+                        <div className="d-flex text-center py-3 justify-content-center">
+                            <i className="fas fa-sun text-white fa-7x px-2"></i>
+                            <h1 className="display-4 text-white">28°C</h1>
+                        </div>
+                    </div>
+                    <div className="foot py-4">
+                        <div className="foo_head">
+                            <h4 className="text-center">Wednesday, 12:00 PM</h4>
+                        </div>
+                        <div className="foot_body table-responsive">
+                            <table className="table  text-center mt-2">
+                                <tbody>
+                                  <tr>
+                                    <th scope="row">Humidity</th>
+                                    <td>63%</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Pressure</th>
+                                    <td>1015 mb</td>
+                                </tr>
+                                  <tr>
+                                    <th scope="row">Visibility</th> 
+                                    <td>1.6 km</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Wind</th>
+                                    <td>ENE 3 km/h</td>
+                                </tr>
+                                </tbody>
+                              </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
